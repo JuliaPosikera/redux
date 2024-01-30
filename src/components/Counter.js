f9dd3a9f720e7dbbdff50d3fb0e67d9a84fe9941
JuliaPosikera <posikera.julia@gmail.com>
2024-01-30 21:55:55 +0100

    simple redux icrementing

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,33 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  const counter = useSelector((state) => state.counter);
+  const toggle = useSelector((state) => state.showCounter);
+  const dispatch = useDispatch();
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggleCounter" });
+  };
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+  const increaseHandler = () => {
+    dispatch({ type: "increase", value: 5 });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {toggle && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Incremet</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./store";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
